1831e9811f99a879ae1c1beafa903bea52af13a0
alvyang <626183528@qq.com>
2017-05-11 13:29:56 +0800

    index.js

--- src/index.js
+++ src/index.js
@@ -2,28 +2,8 @@ import AddressSelect from '../packages/address-select/index.js';
 import Prompt from '../packages/prompt/index.js';
 import SliderDelete from '../packages/slider-delete/index.js';
 
-const components = [
-	AddressSelect,
-	Prompt,
-	SliderDelete,
-]
-
-const install = function(Vue, opts = {}) {
-  /* istanbul ignore if */
-  if (install.installed) return;
-  components.map(component => {
-    Vue.component(component.name, component);
-  });
-};
-
-/* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-};
-
 module.exports = {
 	AddressSelect,
-	install,
 	Prompt,
 	SliderDelete,
 }
