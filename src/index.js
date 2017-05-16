2870ee0e6adb63be388bd83bc0c8edc158f0289f
alvyang <626183528@qq.com>
2017-05-16 16:40:56 +0800

    test complate

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import AddressSelect from '../packages/address-select/index.js';
 import Prompt from '../packages/prompt/index.js';
 import SliderDelete from '../packages/slider-delete/index.js';
 
+const components = [
+	AddressSelect,
+	Prompt,
+	SliderDelete,
+];
+
+const install = function(Vue,options){
+	if (install.installed) return;
+	components.map(component => {
+	    Vue.component(component.name, component);
+	});
+}
+
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+};
+
 module.exports = {
+	install,
 	AddressSelect,
 	Prompt,
 	SliderDelete,
